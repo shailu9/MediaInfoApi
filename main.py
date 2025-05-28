49a8493f7afc895753514d088b6b19935f787f20
Shailesh Joshi <shailesh.joshi090@gmail.com>
2025-05-28 09:36:21 -0400

    removed unused endpoint

--- main.py
+++ main.py
@@ -12,11 +12,6 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
 class ProbeRequest(BaseModel):
     url: str
 
